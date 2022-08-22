7264a2b4dedf2b96616231cc6482b0007f4f9acd
Andrew Nguyen-Tran <ann013@ucsd.edu>
2022-08-21 23:54:41 -0700

    first commit

---
+++ LIB/Employee.js
@@ -0,0 +1,9 @@
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = "Employee" 
+    }
+}
+module.exports = Employee;
---
+++ index.js
@@ -0,0 +1,131 @@
+const inquirer = require("inquirer")
+const Manager = require("./LIB/Manager")
+
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "direction",
+                message: "What type of employee would you like to add?",
+                choices: ["Manager", "Engineer", "Intern", "Done"]
+
+            }
+        ]).then((result) => {
+            if (result.direction === "Manager") {
+                askManager()
+            } else if (result.direction === "Engineer") {
+                askEngineer()
+            } else if (result.direction === "Intern") {
+                askIntern()
+            } else { console.log("Goodbye") }
+        }).catch((err) => {
+            console.log(err)
+        });
+
+}
+
+function askManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What the name of the employee?"
+
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What the ID of the employee?"
+
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "What the email of the employee?"
+
+        },
+
+        {
+            type: "input",
+            name: "officenumber",
+            message: "What the office number of the employee?"
+
+        }
+
+    ]).then(result => {const manager = new Manager(result.name, result.id, result.email, result.officenumber)
+    console.log(manager)
+    })
+
+}
+
+function askEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What the name of the employee?"
+
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "What the ID of the employee?"
+
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "What the email of the employee?"
+
+        },
+
+        {
+            type: "input",
+            name: "gitHub",
+            message: "What the Github username of the employee?"
+
+        }
+
+    ])
+
+}
+
+function askIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What the name of the employee?"
+
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "What the ID of the employee?"
+
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "What the email of the employee?"
+
+        },
+
+        {
+            type: "input",
+            name: "school",
+            message: "What the school of the employee?"
+
+        }
+
+    ])
+
+}
+
+
+
+mainMenu()
